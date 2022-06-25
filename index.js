70e2a75e31af2d7b64a2655f7bcc9f15aa8010aa
valentinadinnadedeiokai <valentinaokai@gmail.com>
2022-06-25 14:06:23 -0700

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(){
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return ['Antonia', 'Nuru'];
+}
+const returnLastTwoDrivers = function(){
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return ['Amari', 'Mo']
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = function (fareMultiplier) {
+    return function(price) {
+      return fareMultiplier * price;
+    }
+  }
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, func) {
+    return func(drivers);
+  }
